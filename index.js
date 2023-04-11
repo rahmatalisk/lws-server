887f1d86c4e9d63c1a80b219c45da920a7cc9cfb
rahmatalisk <sk.rahmatali6792@gmail.com>
2023-04-11 22:07:37 +0600

    added

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const jsonServer = require("json-server"); // importing json-server library
-const auth = require("json-server-auth"); // importing json-server-auth library
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const bodyParser = require("body-parser");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 9000; // you can use any port number here; I chose to use 9000
+const port = process.env.PORT || 9000;
 
-server.db = router.db; // needed for json-server-auth
+server.db = router.db;
 
 server.use(middlewares);
 server.use(auth);
 server.use(router);
+
+// Use body-parser to parse incoming request bodies
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
