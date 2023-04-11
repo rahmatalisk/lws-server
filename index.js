0460b5e514ef8e23a5f9dc6d58ae3205e9ca22be
rahmatalisk <sk.rahmatali6792@gmail.com>
2023-04-11 21:58:24 +0600

    added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const jsonServer = require("json-server"); // importing json-server library
+const auth = require("json-server-auth"); // importing json-server-auth library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 9000; // you can use any port number here; i chose to use 3001
+const port = process.env.PORT || 9000; // you can use any port number here; I chose to use 9000
+
+server.db = router.db; // needed for json-server-auth
 
 server.use(middlewares);
+server.use(auth);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
